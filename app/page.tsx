4f2d86fa641c31c806852cbe6c79a96d36c480cb
lzpel <18492524+lzpel@users.noreply.github.com>
2024-10-16 04:10:56 +0900

    horizon

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 import React from 'react'
-import {Canvas, extend} from '@react-three/fiber'
+import {Canvas, extend, useFrame} from '@react-three/fiber'
 import {MeshLineGeometry, MeshLineMaterial} from 'meshline'
 import {OrbitControls} from '@react-three/drei'
 import * as topojson from "topojson-client"
 import atlas from "world-atlas/countries-50m.json"
-import {Vector3} from "three";
+import {SVGRenderer} from 'three/addons/renderers/SVGRenderer.js';
+import { Vector3, Quaternion } from 'three'
 
 extend({MeshLineGeometry, MeshLineMaterial})
 
@@ -19,21 +20,21 @@ function convertVertex(latitude: number, longitude: number) {
 }
 
 export default function Home() {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-expect-error
-    const country = topojson.mesh(atlas, atlas.objects.countries).coordinates
+	const country = topojson.mesh(atlas, atlas.objects.countries).coordinates
 	const country_v = country.map(v => v.map(v => convertVertex(v[0], v[1])))
-	const global_v=new Float32Array(country_v.flat(2))
+	const global_v = new Float32Array(country_v.flat(2))
 	const indices = country.map(v => v.length).map((v, i, a) => {
 		const offset = a.slice(0, i).reduce((a, b) => a + b, 0)
 		return [...Array(v - 1)].map((_, i) => [offset + i, offset + i + 1])
 	}).flat()
 	console.log(JSON.stringify(indices))
-	const global_i=new Uint32Array(indices.flat())
+	const global_i = new Uint32Array(indices.flat())
 	//Material with color = 'white' is not white https://github.com/pmndrs/react-three-fiber/discussions/1290#discussioncomment-668649
 	//使いたい　https://github.com/spite/THREE.MeshLine?tab=readme-ov-file
 	//データだけ引っ張ってblenderでキービジュアルを作ろう
-	const chiangmai=new Vector3(...convertVertex(98.98, 18.73))
+	const chiangmai = false
 	/*
 	const meshline=country.map((v, i) =>
 		<mesh onPointerOver={console.log} key={i}>
@@ -41,15 +42,26 @@ export default function Home() {
 			<meshLineMaterial color="#ddd" lineWidth={0.002}/>
 		</mesh>
 	)*/
+	const lineMaterial = <lineBasicMaterial color={"#ddd"} linewidth={2}/>
 	return (
 		<Canvas
 			style={{width: "100%", height: "100%"}}
 			linear
 			flat
-			camera={{position: [1,1,-1], near: 0.01}}
-			gl={{antialias: false}}
+			camera={{position: [1, 1, -1], near: 0.001, fov: 90}}
+			gl={chiangmai ? ((canvas) => {
+				const gl = new SVGRenderer()
+				if ("parentNode" in canvas) {
+					const parent = canvas.parentNode
+					if (parent) {
+						parent.removeChild(canvas)
+						parent.appendChild(gl.domElement)
+					}
+				}
+				return gl
+			}) : {antialias: false}}
 		>
-			<OrbitControls target={chiangmai}/>
+			<OrbitControls target={chiangmai ? new Vector3(...convertVertex(98.98, 18.73)):undefined}/>
 			<lineSegments scale={1}>
 				<bufferGeometry>
 					<bufferAttribute
@@ -65,17 +77,67 @@ export default function Home() {
 						itemSize={1}
 					/>
 				</bufferGeometry>
-				<lineBasicMaterial color={"#ddd"} linewidth={3}/>
+				{lineMaterial}
 			</lineSegments>
-			<mesh>
-				<sphereGeometry args={[1, 24, 24]}/>
-				<meshBasicMaterial color={"white"}/>
-			</mesh>
-			<mesh>
-				<sphereGeometry args={[-1.001, 24, 24]}/>
-				<meshBasicMaterial color={"#ddd"}/>
-			</mesh>
-			<ambientLight color={"#ffffff"} intensity={1}/>
+			<Horizon
+				radius={1}
+				faceMaterial={<meshBasicMaterial color={"white"} side={2}/>}
+				lineMaterial={lineMaterial}
+			/>
 		</Canvas>
 	);
+}
+const Horizon = (props: {
+	radius: number,
+	faceMaterial: React.ReactNode,
+	lineMaterial: React.ReactNode
+}) => {
+	const [pos, setPos] = React.useState(new Vector3())
+	useFrame((state) => {
+		setPos(new Vector3(state.camera.position.x, state.camera.position.y, state.camera.position.z))
+	});
+	const angle_cos = Math.min(props.radius / pos.length(), 1)
+	const angle_sin = Math.sqrt(1 - angle_cos * angle_cos)
+	const quaternion = new Quaternion();
+	quaternion.setFromUnitVectors(new Vector3(0, 1, 0), pos.normalize().multiplyScalar(-1))
+	const circle_vector=[...Array(100)].map((_,i,a)=>i/a.length*2*Math.PI).map(v=>[Math.sin(v), 1, Math.cos(v)])
+	const circle_index=circle_vector.map((_,i,a)=>[i,(i+1)%a.length]).flat()
+	const circle_index2=circle_vector.map((_,i,a)=>[i,0,(i+1)%a.length]).slice(1).flat()
+	const scale=new Vector3(angle_sin, -angle_cos, angle_sin).multiplyScalar(props.radius)
+	return <>
+		<mesh quaternion={quaternion} scale={scale}>
+			<bufferGeometry>
+				<bufferAttribute
+					attach='attributes-position'
+					array={new Float32Array(circle_vector.flat())}
+					count={circle_vector.length}
+					itemSize={3}
+				/>
+				<bufferAttribute
+					attach='index'
+					array={new Uint32Array(circle_index2)}
+					count={circle_index2.length}
+					itemSize={1}
+				/>
+			</bufferGeometry>
+			{props.faceMaterial}
+		</mesh>
+		<lineSegments quaternion={quaternion} scale={scale}>
+			<bufferGeometry>
+				<bufferAttribute
+					attach='attributes-position'
+					array={new Float32Array(circle_vector.flat())}
+					count={circle_vector.length}
+					itemSize={3}
+				/>
+				<bufferAttribute
+					attach='index'
+					array={new Uint32Array(circle_index)}
+					count={circle_index.length}
+					itemSize={1}
+				/>
+			</bufferGeometry>
+			{props.lineMaterial}
+		</lineSegments>
+	</>
 }
