975a3d83bca53669bf9a8e75bce7034f846661e0
lzpel <18492524+lzpel@users.noreply.github.com>
2024-10-18 12:28:50 +0900

    replace glsl to useRef

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
-import React from 'react'
-import {Canvas, extend} from '@react-three/fiber'
+import React, {useRef} from 'react'
+import {Canvas, extend, useFrame} from '@react-three/fiber'
 import {MeshLineGeometry, MeshLineMaterial} from 'meshline'
 import {OrbitControls} from '@react-three/drei'
 import * as topojson from "topojson-client"
 import atlas from "world-atlas/countries-50m.json"
 import {SVGRenderer} from 'three/addons/renderers/SVGRenderer.js';
-import { Vector3 } from 'three'
+import {Group, Vector3} from 'three'
 
 extend({MeshLineGeometry, MeshLineMaterial})
 
@@ -60,8 +60,8 @@ export default function Home() {
 				return gl
 			}) : {antialias: false}}
 		>
-			<OrbitControls target={true ? new Vector3(...convertVertex(98.98, 18.73)):undefined}/>
-			<lineSegments scale={1}>
+			<OrbitControls target={chiangmai ? new Vector3(...convertVertex(98.98, 18.73)) : undefined}/>
+			<lineSegments scale={2}>
 				<bufferGeometry>
 					<bufferAttribute
 						attach='attributes-position'
@@ -79,57 +79,32 @@ export default function Home() {
 				{lineMaterial}
 			</lineSegments>
 			<Horizon
-				radius={1}
+				radius={2}
 				faceMaterial={<meshBasicMaterial color={"white"} side={2}/>}
 				lineMaterial={lineMaterial}
 			/>
 		</Canvas>
 	);
 }
-//EX,EY,EZは直行していないので若干歪むのかも
-//グランシュミットの直交化などでどうにかするか？
-//(X-O).xyz=PV*vec4(0.01, 0.0, 0.0, 0.0)で元の位置の情報が消えている
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const vertexShader=`void main() {
-	vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    mat4 PV = projectionMatrix * viewMatrix;
-    vec4 X = PV * vec4(0.01, 0.0, 0.0, 1.0);
-    vec4 Y = PV * vec4(0.0, 0.01, 0.0, 1.0);
-    vec4 Z = PV * vec4(0.0, 0.0, 0.01, 1.0);
-    vec4 O = PV * vec4(0.0, 0.0, 0.0, 1.0);
-    vec3 EX = normalize((X-O).xyz);
-    vec3 EY = normalize((Y-O).xyz);
-    vec3 EZ = normalize((Z-O).xyz);
-    mat4 R=transpose(mat4(
-        vec4(EX, 0.0),
-        vec4(EY, 0.0),
-        vec4(EZ, 0.0),
-        vec4(0.0, 0.0, 0.0, 1.0)
-    ));
-	gl_Position = PV * R * modelPosition;
-}`
-//直交化と投影に含まれる回転も含めて上手くいっている感がある
-//ただ四次元行列の逆行列の計算コストがきになる
-const vertexShader2=`void main() {
-	vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    mat4 PV = projectionMatrix * viewMatrix;
-    mat4 IPV = inverse(PV);
-    vec3 EZ = normalize((IPV * vec4(0,0,1,0)).xyz);
-    vec3 EY_ = (IPV * vec4(0,1,0,0)).xyz;
-    vec3 EY = normalize(EY_ - dot(EY_, EZ)*EZ);
-    vec3 EX = cross(EZ, EY);
-    mat4 R = mat4(mat3(EX,EY,EZ));
-	gl_Position = PV * R * modelPosition;
-}`
 const Horizon = (props: {
 	radius: number,
 	faceMaterial: React.ReactNode,
 	lineMaterial: React.ReactNode
 }) => {
-	const circle_vector=[...Array(100)].map((_,i,a)=>i/a.length*2*Math.PI).map(v=>[Math.cos(v), Math.sin(v), -1])
-	const circle_index=circle_vector.map((_,i,a)=>[i,(i+1)%a.length]).flat()
-	const circle_index2=circle_vector.map((_,i,a)=>[0,i,(i+1)%a.length]).slice(1).flat()
-	return <>
+	const ref = useRef<Group>(null);
+	useFrame((state) => {
+		if (ref.current) {
+			const camera_to_object = ref.current.position.clone().sub(state.camera.position)
+			const angle_cos = Math.min(props.radius / camera_to_object.length(), 1)
+			const angle_sin = Math.sqrt(1 - angle_cos * angle_cos)
+			ref.current.scale.set(props.radius*angle_sin, props.radius*angle_sin, props.radius*angle_cos)
+			ref.current.lookAt(state.camera.position);
+		}
+	})
+	const circle_vector = [...Array(100)].map((_, i, a) => i / a.length * 2 * Math.PI).map(v => [Math.cos(v), Math.sin(v), 1])
+	const circle_index = circle_vector.map((_, i, a) => [i, (i + 1) % a.length]).flat()
+	const circle_index2 = circle_vector.map((_, i, a) => [0, i, (i + 1) % a.length]).slice(1).flat()
+	return <group ref={ref}>
 		<mesh>
 			<bufferGeometry>
 				<bufferAttribute
@@ -163,7 +138,6 @@ const Horizon = (props: {
 				/>
 			</bufferGeometry>
 			{props.lineMaterial}
-			<shaderMaterial vertexShader={vertexShader2}/>
 		</lineSegments>
-	</>
+	</group>
 }
